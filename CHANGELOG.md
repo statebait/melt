542b81f6f8fb52c015f20b692c0b1b9ef21a3d04
Mohamed Shadab <mohamedshadab78@gmail.com>
2019-11-22 16:32:08 +0530

    feat: configure to disable warn prompt 🔥

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,12 +4,13 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### [0.0.1-alpha.2](https://github.com/statebait/melt/compare/v0.0.1-alpha.1...v0.0.1-alpha.2) (2019-11-19)
 
-### 0.0.1-alpha.1 (2019-11-18)
+Release fixes
 
+### 0.0.1-alpha.1 (2019-11-18)
 
 ### Features
 
-* use melt using npm/yarn ([2d95e94](https://github.com/statebait/melt/commit/2d95e943d727622ee2984e6cc168e64c368ad8b9))
+- use melt using npm/yarn ([2d95e94](https://github.com/statebait/melt/commit/2d95e943d727622ee2984e6cc168e64c368ad8b9))
 
 ### 0.0.1-alpha.0 (2019-11-17)
 
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import rimraf from "rimraf";
 import fs from "fs";
 
 // Types
-type AnswersType = { consent: String };
+type AnswersType = { consent: string };
 
 // Message if no path is passed
 const NO_PATH_MESSAGE =
@@ -19,44 +19,69 @@ program.version(
 );
 
 // CLI Options
-program.option(
-  "-r, --recursive",
-  "recursively delete a folder and its contents"
-);
+program
+  .option("-r, --recursive", "recursively delete a folder and its contents")
+  .option("-p, --prompt", "Set 'Are you sure' prompt to on or off.");
 
 /*
 Parses the passed arguments and stores them in program.args (regular arguments passed) and program.opts() (options passed)
 */
 program.parse(process.argv);
 
-// Path constant
-
 // Main function
 (function() {
-  const PATH = program.args[0];
+  // Path constant
+  const MAIN_ARG: string = program.args[0];
+  // Path to the config file; .meltrc.json
+  const pathToConfig: string =
+    __dirname.slice(0, __dirname.length - 6) + "/.meltrc.json";
+
   // Checks if path argument is passed or not
   if (program.args.length === 0) {
     return console.log(NO_PATH_MESSAGE);
   }
 
+  if (program.prompt) {
+    let temp: boolean;
+    if (MAIN_ARG.toLowerCase() === "off") {
+      temp = false;
+    } else if (MAIN_ARG.toLowerCase() === "on") {
+      temp = true;
+    } else {
+      return console.log("Incorrect argument passed.");
+    }
+    fs.writeFileSync(pathToConfig, JSON.stringify({ prompt: temp }));
+    return console.log(`Prompt set to ${MAIN_ARG.toLowerCase()}.`);
+  }
+
   if (program.recursive) {
-    inquirer
-      .prompt([
-        {
-          name: "consent",
-          message:
-            "This will delete the folder and all its contents. Are you sure you want to proceed(Y/n)?\n"
-        }
-      ])
-      .then((answers: AnswersType) => {
-        if (answers.consent[0].toLowerCase() === "y") {
-          return rimraf.sync(PATH);
-        } else if (answers.consent[0].toLowerCase() === "n") {
-          return console.log("Folder not deleted.");
-        }
-      });
-  } else
-    return fs.unlink(PATH, error => {
+    let promptFlag: boolean = true;
+    if (fs.existsSync(pathToConfig)) {
+      const data = fs.readFileSync(pathToConfig);
+      const { prompt } = JSON.parse(data.toString());
+      promptFlag = prompt;
+    }
+    if (promptFlag) {
+      inquirer
+        .prompt([
+          {
+            name: "consent",
+            message:
+              "This will delete the folder and all its contents. Are you sure you want to proceed(Y/n)?\n"
+          }
+        ])
+        .then((answers: AnswersType) => {
+          if (answers.consent[0].toLowerCase() === "y") {
+            return rimraf.sync(MAIN_ARG);
+          } else if (answers.consent[0].toLowerCase() === "n") {
+            return console.log("Folder not deleted.");
+          }
+        });
+    } else {
+      return rimraf.sync(MAIN_ARG);
+    }
+  } else {
+    return fs.unlink(MAIN_ARG, error => {
       if (error) {
         return console.log(
           "Something went wrong. Please use -r if deleting a folder."
@@ -64,4 +89,5 @@ program.parse(process.argv);
       }
       return;
     });
+  }
 })();
